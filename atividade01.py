240879c5c4566b597133bfec1d3e3ad218707ee9
fannymuniz11 <munizfanny03@gmail.com>
2024-09-04 15:26:11 -0300

    first commit

---
+++ atividade01.py
@@ -0,0 +1,28 @@
+# Crie um programa que receba dois números do usuário e exiba a soma desses números.
+val1 = int(input("digite o 1 valor"))
+val2 = int(input("digite o 2 valor"))
+
+
+soma = val1 + val2
+
+print(F" a soma do valor é = {soma}')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
